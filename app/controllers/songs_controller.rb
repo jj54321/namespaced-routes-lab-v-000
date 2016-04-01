271ce29bd709d2389bdabb0123f09d3144de8814
jj54321 <mailme711@gmail.com>
2016-04-01 11:31:28 -0400

    troubleshoot last test

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,8 +24,14 @@ class SongsController < ApplicationController
     end
   end
 
+
   def new
-    @song = Song.new
+    pref = Preference.first
+    if pref && !pref.allow_create_songs
+      redirect_to songs_path
+    else
+      @song = Song.new
+    end
   end
 
   def create
@@ -67,4 +73,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
